4f12781b1c916d3063a440c1561d9211a4302f46
yangweihong <1317018337@qq.com>
2023-04-25 23:04:44 +0800

    1.完成usermapper的编写 2.完成usermapper的测试 3.初步设置日志打印级别为debug

---
+++ src/test/java/com/communityforum/MapperTests.java
@@ -0,0 +1,56 @@
+package com.communityforum;
+
+
+import com.communityforum.dao.UserMapper;
+import com.communityforum.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+@SpringBootTest
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    void testSelectUser() {
+        User user = userMapper.selectById(150);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    void testInsertUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("niuke@sina.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    void updateUser() {
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+    }
+}
