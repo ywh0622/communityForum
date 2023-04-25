54aaf334dcca80c430892a70f0fbb631841461ba
yangweihong <1317018337@qq.com>
2023-04-25 16:57:39 +0800

    usermapper first commit

--- src/main/java/com/communityforum/dao/UserMapper.java
+++ src/main/java/com/communityforum/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.communityforum.dao;
 
 
+import com.communityforum.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -10,5 +11,18 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface UserMapper {
+    User selectById(int id);
+
+    User selectByName(String name);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
+
+    int updateHeader(int id, String headerUrl);
+
+    int updatePassword(int id, String password);
 
 }
