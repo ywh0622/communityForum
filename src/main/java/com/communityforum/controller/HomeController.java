4f9f8c042eb9ecdbdd124e1dd90517cacdcbb1dc
yangweihong <1317018337@qq.com>
2023-04-26 14:07:38 +0800

    2023/4/26提交 1.完成网站首页的显示功能 2.完成分页显示功能 3.初步完成日志相关设置

--- src/main/java/com/communityforum/controller/HomeController.java
+++ src/main/java/com/communityforum/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.communityforum.controller;
 
+import com.communityforum.entity.DiscussPost;
+import com.communityforum.entity.Page;
+import com.communityforum.entity.User;
 import com.communityforum.service.DiscussPostService;
 import com.communityforum.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -16,8 +21,24 @@ public class HomeController {
     private UserService userService;
 
     @GetMapping("/index")
-    public String getIndexPage(Model model){
-
+    public String getIndexPage(Model model, Page page) {
+        // 方法调用前，springmvc会自动实例化model和page，并且将page注入model
+        // 所以thymleaf中可以直接访问Page对象中的数据
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+        System.out.println(page.getCurrent() + " " + page.getOffset() + " " + page.getLimit());
+        List<DiscussPost> posts = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (posts != null) {
+            for (DiscussPost post : posts) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts", discussPosts);
         return "/index";
     }
 }
---
+++ src/test/java/com/communityforum/LoggerTest.java
@@ -0,0 +1,26 @@
+package com.communityforum;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author YWH
+ * @Description 日志测试
+ * @Date 2023/4/26 10:48
+ */
+@SpringBootTest
+public class LoggerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLogger(){
+        System.out.println(logger.getName());
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+}
